eb29f9fe2fae96b709ceed1206d43658dd83955d
Erickson Joseph <erickson1.joseph@gmail.com>
2017-08-25 20:57:47 -0400

    Created bootstrap and local config files for URL stuff

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	<link rel="stylesheet" type="text/css" href="login.css">
 </head>
 <body>
-	<form method="POST" action="/DestinieFoundation/Git/Destinie-Foundation/login_action.php">
+	<form method="POST" action="<?= ROOT_URL ?>login_action.php">
 		<input type="text" name="username" placeholder="username" required>
 		<input type="text" name="password" placeholder="password" required>
 		<input type="submit" value="Login">
